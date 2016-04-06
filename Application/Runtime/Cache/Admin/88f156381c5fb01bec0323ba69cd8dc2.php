6d6a4dcf99e54b80f6320c28d61a9d964e12e540
satansec <1562308723@qq.com>
2016-04-06 13:48:33 +0800

    remove runtime files

--- Application/Runtime/Cache/Admin/88f156381c5fb01bec0323ba69cd8dc2.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<script src="http://www.qq.com/404/search_children.js" charset="utf-8" homePageUrl="<?php echo ($backurl); ?>" homePageName="回到我的主页"></script>
-</head>
-<body></body>
-</html>
